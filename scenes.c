ce03ac358639ae295d630ebb67fdb58b319b4495
Laarabi Suleyman <suleyman.laarabi.dev@gmail.com>
2024-08-23 00:05:25 +0200

    features scenes management

--- scenes.c
+++ scenes.c
@@ -5,3 +5,38 @@
 ** scenes
 */
 
+#include "scenes.h"
+#include <stdlib.h>
+#include <SFML/Audio.h>
+
+scene_entity *create_scene(
+    list_entity *entities,
+    scene_init_func init_func,
+    scene_update_func update_func,
+    char *music_path
+)
+{
+    scene_entity *scene = malloc(sizeof(scene_entity));
+
+    scene->entities = entities;
+    scene->on_init = init_func;
+    scene->on_update = update_func;
+    if (music_path) {
+        scene->music = sfMusic_createFromFile(music_path);
+        sfMusic_play(scene->music);
+    } else {
+        scene->music = NULL;
+    }
+    return scene;
+}
+
+void register_entity(scene_entity *scene, game_entity *entity)
+{
+    scene->entities = push_back_entity(scene->entities, entity);
+}
+
+void load_scene(game_engine *engine, scene_entity *scene)
+{
+    scene->on_init(scene);
+    engine->scene = scene;
+}
--- scenes.h
+++ scenes.h
@@ -7,6 +7,7 @@
 
 #ifndef INCLUDED_SCENES_H
     #define INCLUDED_SCENES_H
+    #include "engine.h"
     #include "lib/list/list.h"
 
 struct scene_entity;
@@ -18,8 +19,17 @@ typedef struct scene_entity {
     list_entity *entities;
     scene_update_func on_update;
     scene_init_func on_init;
-    sfRenderWindow *window;
+    sfMusic *music;
 } scene_entity;
 
+scene_entity *create_scene(
+    list_entity *entities,
+    scene_init_func init_func,
+    scene_update_func update_func,
+    char *music_path
+);
+
+void register_entity(scene_entity *scene, game_entity *entity);
+void load_scene(game_engine *engine, scene_entity *scene);
 
 #endif
